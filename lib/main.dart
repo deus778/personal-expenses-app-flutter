7ba9a09e758fbaaceb57a39bfb3991154d76d571
Irman <irman_omerovic@hotmail.de>
2022-08-05 16:11:45 +0200

    Add transaction model, implement custom list

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './transaction.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -13,14 +15,57 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: "T1",
+      title: "New Shoes",
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "T2",
+      title: "Weekly Groceries",
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Center(
-        child: Text('Widget Playground!'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text("CHART!"),
+              elevation: 5,
+            ),
+          ),
+          Column(
+            children: transactions.map((e) {
+              return Card(
+                child: Row(children: [
+                  Container(
+                    child: Text(e.amount.toString()),
+                  ),
+                  Column(
+                    children: [
+                      Text(e.title!),
+                      Text(e.date.toString()),
+                    ],
+                  )
+                ]),
+              );
+            }).toList(),
+          ),
+        ],
       ),
     );
   }
